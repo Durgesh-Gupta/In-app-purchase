9f4959fe7267ff56afc9b717a9ad765a68b2be61
Durgesh Gupta <durgesh10gupta@gmail.com>
2022-04-17 11:56:06 +0530

    feat:changes for verifyreceipt endpoint

--- controller.js
+++ controller.js
@@ -9,26 +9,93 @@ router.post("/checkreceipt", async (req, res) => {
   let Receipt = req.body || Receipt;
 
   try {
-    if (!Receipt?.originalTransactionId) {
-      throw new Error("Not a Valid Receipt!");
-    }
-    let payload = {
-      iss: "57246542-96fe-1a63e053-0824d011072a",
-      iat: 1623085200,
-      exp: Math.floor(Date.now() / 1000) + 60 * 60,
-      aud: "appstoreconnect-v1",
-      bid: "com.example.testbundleid2021",
+    // if (!Receipt?.originalTransactionId) {
+    //   throw new Error("Not a Valid Receipt!");
+    // }
+    // let payload = {
+    //   iss: "57246542-96fe-1a63e053-0824d011072a",
+    //   iat: 1623085200,
+    //   exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    //   aud: "appstoreconnect-v1",
+    //   bid: "com.example.testbundleid2021",
+    // };
+
+    // let response = await getSubscription(
+    //   Receipt.originalTransactionId,
+    //   payload
+    // );
+    // if (response?.data[0]?.lastTransactions[0].status == 1) {
+    //   res.json({ sucessCode: 201, data: "Subscription is Valid" });
+    // } else {
+    //   res.json({ sucessCode: 201, data: "Subscription is expire!" });
+    // }
+
+    var url = true ? "buy.itunes.apple.com" : "sandbox.itunes.apple.com";
+    var receiptEnvelope = {
+      "receipt-data": Receipt,
+      password: process.env.password,
+      "exclude-old-transactions": false,
+    };
+    var receiptEnvelopeStr = JSON.stringify(receiptEnvelope);
+    var options = {
+      host: url,
+      port: 443,
+      path: "/verifyReceipt",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": Buffer.byteLength(receiptEnvelopeStr),
+      },
     };
 
-    let response = await getSubscription(
-      Receipt.originalTransactionId,
-      payload
-    );
-    if (response?.data[0]?.lastTransactions[0].status == 1) {
-      res.json({ sucessCode: 201, data: "Subscription is Valid" });
-    } else {
-      res.json({ sucessCode: 201, data: "Subscription is expire!" });
+    var response = await axios.post(`${url}/verifyReceipt`, receiptEnvelope, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": Buffer.byteLength(receiptEnvelopeStr),
+      },
+    });
+    let message;
+    switch (response.status) {
+      case 0:
+        message = "Receipt is valid";
+        break;
+      case 21002:
+        message =
+          "The data in the receipt-data property was malformed or the service experienced a temporary issue. Try again.";
+        break;
+      case 21003:
+        message = "The receipt could not be authenticated.";
+        break;
+      case 21004:
+        message =
+          "The shared secret you provided does not match the shared secret on file for your account.";
+        break;
+      case 21005:
+        message =
+          "The receipt server was temporarily unable to provide the receipt. Try again.";
+        break;
+      case 21006:
+        message = "This receipt is valid but the subscription has expired.";
+        break;
+      case 21007:
+        message =
+          "This receipt is from the test environment, but it was sent to the production environment for verification.";
+        break;
+      case 21008:
+        message =
+          "This receipt is from the production environment, but it was sent to the test environment for verification.";
+        break;
+      case 21009:
+        message = "Internal data access error. Try again later.";
+        break;
+      case 21010:
+        message = "The user account cannot be found or has been deleted.";
+        break;
+
+      default:
+      // code block
     }
+    res.json({ sucess: true, message: message, data: response });
   } catch (e) {
     console.log(e.message);
     res.json({ sucess: "Failed", message: e.message });
